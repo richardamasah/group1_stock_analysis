543a8fffcaf05a742218d7e34d25860d30509b6b
djanie <richard.amasah@amalitechtraining.org>
2025-04-04 05:34:52 +0000

    Write functions to load and preprocess dataset

--- src/visualization.py
+++ src/visualization.py
@@ -2,3 +2,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+def load_data(file_path):
+    import pandas as pd
+    return pd.read_csv(file_path)
+
+
+def clean_data(df):
+    df['Date'] = pd.to_datetime(df['Date'])
+    df = df.sort_values('Date')
+    return df
