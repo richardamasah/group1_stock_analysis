bbae79118fc625600e3a2c368982a112c7112b7e
kwame.ofori-gyau@amalitechtraining.org <kwame.ofori-gyau@amalitechtraining.org>
2025-04-04 07:04:06 +0000

    change of file path of the to github filepath

--- src/pipeline.py
+++ src/pipeline.py
@@ -60,7 +60,7 @@ class DataCleaning_and_EDA_pipeline:
         
 
 
-file_path = "/Users/gyauk/Desktop/DataEngineering/stockanalysis/cleaned-stocks.parquet"  
+file_path = "../data/raw_data/processed/cleaned-stocks.parquet"  
 Pline = DataCleaning_and_EDA_pipeline(file_path)
 Pline.view_data()
 Pline.check_missing_values()
