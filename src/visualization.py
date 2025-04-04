c0eb449c22db0f09b54e555a20db6d6967fcd62e
djanie <richard.amasah@amalitechtraining.org>
2025-04-04 05:38:25 +0000

    plot a correlation heat map

--- src/visualization.py
+++ src/visualization.py
@@ -3,12 +3,35 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def load_data(file_path):
-    import pandas as pd
-    return pd.read_csv(file_path)
+# Load the dataset
+def load_stock_data(path):
+    return pd.read_csv(path)
 
-
-def clean_data(df):
+# Clean & preprocess the dataset
+def preprocess_data(df):
     df['Date'] = pd.to_datetime(df['Date'])
     df = df.sort_values('Date')
     return df
+
+
+# Plot line chart of closing prices
+def plot_closing_price(df):
+    plt.figure(figsize=(12, 6))
+    plt.plot(df['Date'], df['Close'], label='Close Price')
+    plt.title('Stock Closing Price Over Time')
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    plt.grid(True)
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+    # Correlation heatmap
+def plot_correlation_heatmap(df):
+    numeric_df = df.select_dtypes(include='number')
+    corr = numeric_df.corr()
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(corr, annot=True, cmap='coolwarm')
+    plt.title('Correlation Heatmap')
+    plt.tight_layout()
+    plt.show()
